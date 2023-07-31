a0755441776b3dd82f09eb2455d8366f76ddc9ab
f1v3-dev <f1v3_dev@naver.com>
2023-07-31 14:06:58 +0900

    Test & Args

---
+++ practice/MainTest.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class MainTest {
+    
+    // static -> class 안에 있는 어느곳이든 호출 가능
+    static String msg = "Hello, world!";
+
+    public static void f() {
+        System.out.println(msg);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(msg);
+        f();
+    }
+}
