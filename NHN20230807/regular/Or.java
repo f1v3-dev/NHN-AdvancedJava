5dffdbdd998cfe3c6202e66f9a2b9997e409fd22
f1v3-dev <f1v3_dev@naver.com>
2023-08-08 10:25:11 +0900

    2023-08-08

--- NHN20230807/regular/Or.java
+++ NHN20230807/regular/Or.java
@@ -1,7 +1,6 @@
 package NHN20230807.regular;
 
-public final class Or extends BinaryOperationBase implements Expression {
-
+public final class Or extends BinaryOperation implements Expression {
 
     public Or(Expression left, Expression right) {
         super(left, right);
--- NHN20230807/regular/Then.java
+++ NHN20230807/regular/Then.java
@@ -1,6 +1,6 @@
 package NHN20230807.regular;
 
-public final class Then extends BinaryOperationBase implements Expression {
+public final class Then extends BinaryOperation implements Expression {
 
     public Then(Expression left, Expression right) {
         super(left, right);
